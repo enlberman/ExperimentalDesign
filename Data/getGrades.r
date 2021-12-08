73b8679955d05e4f6c691562449ae3f3215e0af3
andrewstier <andrewstier@uchicago.edu>
2021-12-08 14:20:23 -0600

    add id rsa and grader

---
+++ Data/getGrades.r
@@ -0,0 +1,24 @@
+library(testthat)
+library(sodium)
+library(DiagrammeR)
+
+grade_my_hw <- function(x,test_file){
+  encrypted_text2 <- as.raw(as.hexmode((readr::read_lines(test_file))))
+  key2 <- as.raw(as.hexmode((readr::read_lines('/home/expdes/public/id_rsa'))))
+  decrypted_text <- unserialize(simple_decrypt(encrypted_text2, key2))
+  readr::write_file(decrypted_text, 'tmp_test.r')
+  trial_test <- data.frame(testthat::test_file('tmp_test.r', reporter = "minimal"))
+  file.remove('tmp_test.r')
+  trial_test <- trial_test[trial_test$test==x]
+  trial_test$passed
+}
+
+show_grade <- function(p, x){
+  color <- ifelse(p , "green", "red")
+  DiagrammeR::grViz(sprintf("digraph {
+  graph [layout = dot, rankdir = TB]
+  
+  node [shape = rectangle]        
+  rec1 [label = %s color = %s fillcolor = %s style = filled]
+  }", x, color, color),  height = 100)
+}
