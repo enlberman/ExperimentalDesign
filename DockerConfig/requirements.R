b6e12d88342a059f03e16c544624a02f39fcd8a3
andrewstier <andrewstier@uchicago.edu>
2021-12-08 14:21:15 -0600

    add packages for autograder

--- DockerConfig/requirements.R
+++ DockerConfig/requirements.R
@@ -1,1 +1,1 @@
-install.packages(c("janitor", "feather", "patchwork","glmnet","lme4","stargazer","corrplot","ggpubr","car"))
+install.packages(c("janitor", "feather", "patchwork","glmnet","lme4","stargazer","corrplot","ggpubr","car","testthat","sodium","DiagrammeR"))
