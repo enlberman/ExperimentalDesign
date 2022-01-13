9e73aa664339003fcfd10adf599e481aa5a58b70
andrewstier <andrewstier@uchicago.edu>
2022-01-13 10:39:46 -0600

    Update requirements.R

--- DockerConfig/requirements.R
+++ DockerConfig/requirements.R
@@ -1,1 +1,1 @@
-install.packages(c("janitor", "feather", "patchwork","glmnet","lme4","stargazer","corrplot","ggpubr","car","testthat","sodium","DiagrammeR"))
+install.packages(c("janitor", "feather", "patchwork","glmnet","lme4","stargazer","corrplot","ggpubr","car","testthat","sodium","DiagrammeR","rmarkdown"))
