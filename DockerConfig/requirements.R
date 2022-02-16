5261777d64d5309f814c02d721693379a3d1b107
andrewstier <andrewstier@uchicago.edu>
2022-02-16 10:00:27 -0600

    Update requirements.R

--- DockerConfig/requirements.R
+++ DockerConfig/requirements.R
@@ -1,4 +1,5 @@
-install.packages(c("janitor", "feather", "patchwork","glmnet","lme4","stargazer","corrplot","ggpubr","car","testthat","sodium","DiagrammeR","rmarkdown", "knitr","markdown","Hmisc","lmtest","ppcor"))
-install.packages(c( "car", "lme4", "lmtest", "ppcor"))
+install.packages(c("janitor", "feather", "patchwork","glmnet","stargazer","corrplot","ggpubr","testthat","sodium","DiagrammeR","rmarkdown", "knitr","markdown","Hmisc"))
 install.packages("rmarkdown", version = "2.11")
 tinytex::install_tinytex()
+
+install.packages(c( "car", "lme4", "lmtest", "ppcor"))
