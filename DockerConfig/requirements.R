dbd6aa34491cd436afa1aed858236373fca8e429
andrewstier <andrewstier@uchicago.edu>
2022-02-28 11:47:20 -0600

    Update requirements.R

--- DockerConfig/requirements.R
+++ DockerConfig/requirements.R
@@ -1,4 +1,6 @@
 install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+library(cmdstanr)
+cmdstanr::install_cmdstan()
 
 install.packages(c("janitor", "feather", "patchwork","glmnet","stargazer","corrplot","ggpubr","testthat","sodium","DiagrammeR","rmarkdown", "knitr","markdown","Hmisc"))
 install.packages("rmarkdown", version = "2.11")
@@ -6,4 +8,8 @@ tinytex::install_tinytex()
 
 install.packages(c( "car", "lme4", "lmtest", "ppcor"))
 install.packages(c("sjPlot","sjmisc", "glmmTMB"))
-install.packages(c("rethinking","dagitty","CCA"))
+
+install.packages(c("coda","mvtnorm","devtools","loo","dagitty","shape"))
+devtools::install_github("rmcelreath/rethinking")
+
+install.packages(c("CCA"))
